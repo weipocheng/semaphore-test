5d7ee98242110751605e4361861281b8b5e77fd8
weipocheng <weipocheng@starcloudsoft.com.tw>
2025-09-15 15:23:32 +0800

    更新 GCS 提供者配置，將憑證路徑變數更改為 JSON 內容，並調整變數描述

--- GCS/providers.tf
+++ GCS/providers.tf
@@ -1,5 +1,5 @@
 provider "google" {
   project = var.project_id
   region  = var.region
-  credentials = var.gcp_credentials_path != "" ? file(var.gcp_credentials_path) : null
+  credentials = var.gcp_credentials_json
 }
